f132fd9bc14fe2d04f39f8d34a516e9abdd8ce5f
jonlee <jonleeyz@outlook.com>
2022-04-03 11:49:31 +0800

    Implement classes: Doctor, Patient

--- model/Doctor.java
+++ model/Doctor.java
@@ -1,4 +1,52 @@
 package model;
 
-public class Doctor {
+class Doctor {
+    private String id;
+    private String name;
+
+    Doctor(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    String getId() {
+        return id;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Doctor otherDoctor = (Doctor) other;
+        if (otherDoctor.getId().equals(this.id) &&
+            otherDoctor.getName().equals(this.name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 17;
+
+        result = prime * result + id.hashCode();
+        result = prime * result + name.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String output = String.format("");
+
+        return output;
+    }
 }
--- model/Patient.java
+++ model/Patient.java
@@ -1,5 +1,97 @@
 package model;
 
-public class Patient {
+import java.lang.IllegalArgumentException;
+// import java.lang.UnsupportedOperationException;
 
+class Patient {
+    // can be updated if more than one gender is to be recorded
+    enum Gender {
+        M,
+        F
+    }
+
+    private String id;
+    private String name;
+    private int age;
+    private Gender gender;
+
+    Patient(String id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        
+        if (gender == "M") {
+            this.gender = Gender.M;
+        } else if (gender == "F") {
+            this.gender = Gender.F;
+        } else {
+            throw new IllegalArgumentException(
+                String.format("No gender implementation for specified gender: %s.", gender));
+        }
+    }
+
+    String getId() {
+        return id;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    Gender getGender() {
+        return gender;
+
+        /*
+        if (gender == Gender.M) {
+            return "M";
+        } else if (gender == Gender.F) {
+            return "F";
+        } else {
+            throw new UnsupportedOperationException(
+                String.format("Missing implementation for Patient.getGender for specified gender: %s.",
+                              gender.toString()));
+        }
+        */
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Patient otherPatient = (Patient) other;
+        if (otherPatient.getId().equals(this.id) &&
+            otherPatient.getName().equals(this.name) &&
+            otherPatient.getAge() == this.age &&
+            otherPatient.getGender() == this.gender) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 17;
+
+        result = prime * result + id.hashCode();
+        result = prime * result + name.hashCode();
+        result = prime * result + age;
+        result = prime * result + gender.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String output = String.format("");
+
+        return output;
+    }
 }
