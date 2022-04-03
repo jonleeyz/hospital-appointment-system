d017021d5fef9b3219431c206a88be40c7d927f0
jonlee <jonleeyz@outlook.com>
2022-04-03 10:11:46 +0800

    Add skeleton class files: Appointment, Doctor, Patient, HospitalAppointmentSystem

---
+++ model/HospitalAppointmentSystem.java
@@ -0,0 +1,19 @@
+package model;
+
+public class HospitalAppointmentSystem {
+    public bool importCsvData(String csvFilePath) {
+        return false;
+    }
+
+    public DATATYPE getAppointments(Doctor doctorID, DateTime dateTime) {
+        return DATATYPE;
+    }
+
+    public bool fixAppointment(Patient patientID, Doctor doctorID, DateTime dateTime) {
+        return false;
+    }
+    
+    public bool cancelAppointment(Patient patientID, Dcotor doctorID, DateTime dateTime) {
+        return false;
+    }
+}
