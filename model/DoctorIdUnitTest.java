6c4758a4956bfe80f3ec863b03c362c08187d47d
jonlee <jonleeyz@outlook.com>
2022-04-05 16:49:33 +0800

    [DoctorIdUnitTest.java] Add skeleton test

--- model/DoctorIdUnitTest.java
+++ model/DoctorIdUnitTest.java
@@ -5,4 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class DoctorIdUnitTest {
+    @Test
+    void skeletonTest() {
+        assertEquals(2, 2);
+    }
 }
