203c085e85b9f5ec51268232496f0d083d992e23
jonlee <jonleeyz@outlook.com>
2022-04-05 22:01:43 +0800

    Implement unit test suite: PatientId

--- model/PatientIdUnitTest.java
+++ model/PatientIdUnitTest.java
@@ -1,5 +1,44 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
 class PatientIdUnitTest {
+    private String x;
+    private String y;
+    private PatientId patientId1;
+
+    @BeforeEach
+    void init() {
+        x = "x";
+        y = "y";
+        patientId1 = new PatientId(x);
+    }
     
+    /**
+     * Tests the PatientId constructor.
+     * - Unit test is trivial if the format of a PatientId is unrestricted.
+     * - Nevertheless, to faciliate TDD, test will be left in place in case PatientId implementation changes.
+     */
+    @Test
+    void ConstructorTest() {
+        assertEquals(patientId1.id, "x");
+
+        PatientId patientIdBlank = new PatientId("");
+        assertEquals(patientIdBlank.id, "");
+    }
+
+    @Test
+    void EqualsOverrideTest() {
+        PatientId patientId2 = new PatientId(x);
+        Object patientId3 = new PatientId(x);
+        PatientId patientId4 = new PatientId(y);
+
+        assertEquals(patientId1, patientId2);
+        assertEquals(patientId1, patientId3);
+        assertNotEquals(patientId1, patientId4);
+    }
 }
