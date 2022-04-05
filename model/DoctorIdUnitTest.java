8dd1568b0d51b57b40ff513d6d7321377108cf4b
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2022-04-05 16:54:49 +0800

    Add skeleton files: JUnit unit test suites (#3)
    * Add skeleton files: JUnit unit test suites
    
    * [DoctorIdUnitTest.java] Add skeleton test

---
+++ model/DoctorIdUnitTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DoctorIdUnitTest {
+    @Test
+    void skeletonTest() {
+        assertEquals(2, 2);
+    }
+}
