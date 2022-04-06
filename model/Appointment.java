d70793930da228e9dcc72709c01b8591df476c64
jonlee <jonleeyz@outlook.com>
2022-04-06 18:23:48 +0800

    [Appointment.java] Update constructor implementation to handle correctly parsed, but invalid dates

--- model/Appointment.java
+++ model/Appointment.java
@@ -3,10 +3,14 @@ package model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashSet;
 
 class Appointment {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("ddMMyyyy");
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static final Integer[] monthsWithout31stArray = {4, 6, 9, 11};
+    private static final HashSet<Integer> monthsWithout31st = new HashSet<Integer>(Arrays.asList(monthsWithout31stArray));
     
     private AppointmentId appointmentId;
     private PatientId patientId;
@@ -23,6 +27,22 @@ class Appointment {
         this.date = LocalDate.parse(date, DATE_FORMATTER);
         this.time = LocalTime.parse(time, TIME_FORMATTER);
         this.duration = 60;
+        
+        // handles invalid Dates: months that have no 31st
+        if ((monthsWithout31st.contains(this.date.getMonthValue()) && this.date.getDayOfMonth() == 31)) {
+            throw new IllegalArgumentException(String.format(String.join("Parsed date is invalid: Invalid value for ",
+                                                                         "DayOfMonth when MonthOfYear is %d: %d"),
+                                                             this.date.getMonthValue(), this.date.getDayOfMonth()));
+        }
+        // handles invalid Dates: February, for leap and normal years
+        if ((this.date.getMonthValue() == 2) &&
+            ((this.date.getYear() % 4 == 0 && this.date.getDayOfMonth() > 29) ||
+             (this.date.getYear() % 4 == 1 && this.date.getDayOfMonth() > 28))) {
+            throw new IllegalArgumentException(String.format(String.join("Parsed date is invalid: Invalid value for ",
+                                                                         "DayOfMonth when MonthOfYear is %d: %d"),
+                                                             this.date.getMonthValue(), this.date.getDayOfMonth()));
+                
+        }
     }
 
     protected AppointmentId getAppointmentId() {
