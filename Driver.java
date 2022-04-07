bbb9083805a322b1757d83ad70456af0e5b5abb6
jonlee <jonleeyz@outlook.com>
2022-04-07 14:30:34 +0800

    Update HospitalAppointmentSystem method call in Driver

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ import model.HospitalAppointmentSystem;
 
 public class Driver {
     public static void main(String[] args) {
-        HospitalAppointmentSystem demoSystem = new HospitalAppointmentSystem("./dummy.csv");
+        HospitalAppointmentSystem demoSystem = new HospitalAppointmentSystem("../test/data/dummy.csv");
         return;
     }
 }
