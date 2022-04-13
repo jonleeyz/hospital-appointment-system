35040b2554432a7096bdc256c55db45e82399610
jonlee <jonleeyz@outlook.com>
2022-04-13 20:10:52 +0800

    [IndexedAppointmentTable.java] Reimplement: for correctness and exception handling

--- model/tables/IndexedAppointmentTable.java
+++ model/tables/IndexedAppointmentTable.java
@@ -5,14 +5,46 @@ import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
-import model.entities.id.AppointmentId;
+import model.entities.id.Id;
 
-public class IndexedAppointmentTable<T> {
+import model.entities.id.AppointmentId;
+import util.DateTimeParser;
+
+public class IndexedAppointmentTable<T extends Id> {
     private HashMap<T, HashMap<LocalDate, HashMap<LocalTime, AppointmentId>>> table
         = new HashMap<T, HashMap<LocalDate, HashMap<LocalTime, AppointmentId>>>();
 
     public boolean isEmpty() {
         return table.isEmpty();
+    }
+
+    /**
+     * Returns true if there exists an AppointmentId in the table that matches the provided arguments
+     * and false otherwise.
+     */
+    public boolean containsAppointmentId(T id, LocalDate date, LocalTime time) {
+        try {
+            return table.get(id).get(date).get(time) != null;
+        } catch (NullPointerException npe) {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the AppointmentId corresponding to the provided arguments.
+     * @throws NoSuchElementException if no stored AppointmentId matches the provided arguments.
+     */
+    public AppointmentId getAppointmentId(T id, LocalDate date, LocalTime time) throws NoSuchElementException {
+        if (containsAppointmentId(id, date, time)) {
+            return table.get(id).get(date).get(time);
+        } else {
+            throw new NoSuchElementException(String.format(String.join(" ",
+                                                                       "No matching Appointment for given parameters:",
+                                                                       "id %s, date %s, time %s."),
+                                                           id,
+                                                           DateTimeParser.toString(date),
+                                                           DateTimeParser.toString(time)));
+        }
     }
 
     public HashMap<LocalTime, AppointmentId> getAll(T id, LocalDate date) {
@@ -30,29 +62,22 @@ public class IndexedAppointmentTable<T> {
         table.get(id).get(date).put(time, appointmentId);
     }
 
+    /**
+     * Removes the AppointmentId corresponding to the provided arguments.
+     * @throws NoSuchElementException if no stored AppointmentId matches the provided arguments.
+     */
     public void remove(T id, LocalDate date, LocalTime time, AppointmentId appointmentId) throws NoSuchElementException {
-        if (table.get(id).get(date).get(time).equals(appointmentId)) {
-            table.get(id).get(date).remove(time);
-        } else {
-            throw new NoSuchElementException(String.format("No matching Appointment for AppointmentId %s.",
-                                             appointmentId));
+        try {
+            if (getAppointmentId(id, date, time).equals(appointmentId)) {
+                table.get(id).get(date).remove(time);
+                return;
+            } 
+        } catch (NoSuchElementException nsee) {
         }
-    }
-
-    public boolean contains(T id, LocalDate date, LocalTime time) {
-        return table.get(id).get(date).get(time) != null;
-    }
-
-    public AppointmentId getAppointmentId(T id, LocalDate date, LocalTime time) throws NoSuchElementException {
-        if (contains(id, date, time)) {
-            return table.get(id).get(date).get(time);
-        } else {
-            throw new NoSuchElementException(String.format(String.join(" ",
-                                                                       "No matching Appointment for given parameters:",
-                                                                       "id %s, date %s, time %s."),
-                                                           id,
-                                                           date.toString(),
-                                                           time.toString()));
-        }
+        throw new NoSuchElementException(String.format(String.join(" ",
+                                                                   "No matching Appointment for given parameters:",
+                                                                   "id %s, date %s, time %s, AppointmentId %s"),
+                                                       id, DateTimeParser.toString(date),
+                                                       DateTimeParser.toString(time), appointmentId));
     }
 }
