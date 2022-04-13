b176dd133bb5194854e7b8883feafe89d2527a21
jonlee <jonleeyz@outlook.com>
2022-04-13 20:06:21 +0800

    [DateTimeParser.java] Add verifyDateIsValid method to handle date validity checking

--- util/DateTimeParser.java
+++ util/DateTimeParser.java
@@ -5,10 +5,14 @@ import java.time.format.DateTimeParseException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class DateTimeParser {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("ddMMyyyy");
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static final Integer[] monthsWithout31stArray = {4, 6, 9, 11};
+    private static final HashSet<Integer> monthsWithout31st = new HashSet<Integer>(Arrays.asList(monthsWithout31stArray));
 
     public static boolean isParseableAsDate(String date) {
         try {
@@ -42,5 +46,40 @@ public class DateTimeParser {
 
     public static String convertToString(LocalTime time) throws DateTimeException {
         return time.format(TIME_FORMATTER);
+    }
+
+    /**
+     * Ensures that the specified date is valid.
+     * <p>
+     * A date is invalid if:
+     * <ul>
+     * <li>its month value is one of {4, 6, 9, 11} and its day value is 31 or more.</li>
+     * <li>its month value is 2, and its day is 30 or more.</li>
+     * <li>its year value is a non-leap year, its month value is 2, and its day value is 29.</li>
+     * </ul>
+     * and valid otherwise.
+     * 
+     * @return the specified date if it is valid.
+     * @throws IllegalArgumentException if the specified date is invalid.
+     */
+    public static LocalDate verifyDateIsValid(LocalDate date) throws IllegalArgumentException {
+        // handles no 31st month cases
+        if ((monthsWithout31st.contains(date.getMonthValue()) && date.getDayOfMonth() >= 31)) {
+            throw new IllegalArgumentException(String.format(String.join(" ",
+                                                                         "Date is invalid: Illegal value for",
+                                                                         "day when month is %d: %d"),
+                                                             date.getMonthValue(), date.getDayOfMonth()));
+        }
+        // handles February cases
+        if ((date.getMonthValue() == 2) &&
+            (date.getDayOfMonth() >= 30) || (date.getYear() % 4 != 0 && date.getDayOfMonth() == 29)) {
+            throw new IllegalArgumentException(String.format(String.join(" ",
+                                                                         "Date is invalid: Invalid value for",
+                                                                         "day when month is %d",
+                                                                         "and year is %d: %d"),
+                                                             date.getMonthValue(), date.getYear(),
+                                                             date.getDayOfMonth()));
+        }
+        return date;
     }
 }
